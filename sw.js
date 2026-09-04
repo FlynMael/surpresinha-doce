59df12595e878356e77841d6f82e0917ba711a92
FlynMael <consultamael@outlook.com.br>
2026-09-04 00:11:54 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'surpresinha-v1';
+const ARQUIVOS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).catch(() => {}));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copia = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copia)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
